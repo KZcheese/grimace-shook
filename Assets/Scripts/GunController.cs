488d960680dcef6c80507c9bbec6e32ebc7b45eb
Kevin Zhan <kevin.w.zhan@gmail.com>
2023-08-03 22:42:39 -0400

    bunch a stuff

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -7,9 +7,12 @@ public class GunController : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public int ammo = 10;
 
     public Camera gunCam;
     public StarterAssetsInputs input;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
 
     private void Update()
     {
@@ -19,17 +22,19 @@ public class GunController : MonoBehaviour
     private void Fire()
     {
         input.shoot = false;
+        muzzleFlash.Play();
 
         Transform camTransform = gunCam.transform;
         if(!Physics.Raycast(camTransform.position, camTransform.forward, out RaycastHit hit, range)) return;
 
-        Debug.Log(hit.transform.name);
+        // Debug.Log(hit.transform.name);
 
+        GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 2f);
+        
         Target target = hit.transform.GetComponent<Target>();
         if(!target) return;
 
         target.TakeDamage(damage);
-
-
     }
 }
