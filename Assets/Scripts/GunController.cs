6274c5fc4f8479dcc334d4cfc7ad41e2949b3117
Kevin Zhan <kevin.w.zhan@gmail.com>
2023-08-05 00:21:28 -0400

    reloading and gun graphics

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using InputSystem;
 using UnityEngine;
 
@@ -5,36 +6,51 @@ public class GunController : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
-    public int ammo = 10;
-    private int _magazineSize;
+    public int maxAmmo = 10;
+    private int _currentAmmo;
+    public float fireRate = 0.1f;
+    public float reloadTime = 1f;
+    private bool _isReloading;
 
     public Camera gunCam;
+
     public StarterAssetsInputs input;
-    public ParticleSystem muzzleFlash;
+
+    // public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
+    public SpriteRenderer gunRenderer;
+    private Sprite _idleSprite;
+    public Sprite shootingSprite;
+    public Sprite reloadingSprite;
+    public Sprite emptySprite;
 
 
     private void Start()
     {
-        _magazineSize = ammo;
+        _currentAmmo = maxAmmo;
+        _idleSprite = gunRenderer.sprite;
     }
 
     private void Update()
     {
-        if(input.shoot) Fire();
-        if(input.reload) Reload();
+        if(input.shoot) StartCoroutine(Fire());
+        if(input.reload && !_isReloading) StartCoroutine(Reload());
     }
 
-    private void Fire()
+    private IEnumerator Fire()
     {
-        if(ammo < 1) return;
+        if(_currentAmmo < 1) yield break;
 
         input.shoot = false;
-        muzzleFlash.Play();
-        ammo--;
+
+        // muzzleFlash.Play();
+        gunRenderer.sprite = shootingSprite;
+        _currentAmmo--;
+
 
         Transform camTransform = gunCam.transform;
-        if(!Physics.Raycast(camTransform.position, camTransform.forward, out RaycastHit hit, range)) return;
+        if(!Physics.Raycast(camTransform.position, camTransform.forward, out RaycastHit hit, range))
+            yield break;
 
         // Debug.Log(hit.transform.name);
 
@@ -42,13 +58,24 @@ public class GunController : MonoBehaviour
         Destroy(impact, 2f);
 
         Target target = hit.transform.GetComponent<Target>();
-        if(!target) return;
+        if(target) target.TakeDamage(damage);
 
-        target.TakeDamage(damage);
+        yield return new WaitForSeconds(fireRate);
+
+        gunRenderer.sprite = _currentAmmo < 1 ? emptySprite : _idleSprite;
     }
 
-    private void Reload()
+    private IEnumerator Reload()
     {
-        ammo = _magazineSize;
+        gunRenderer.sprite = reloadingSprite;
+        _isReloading = true;
+        
+        yield return new WaitForSeconds(reloadTime);
+        
+        gunRenderer.sprite = _idleSprite;
+        _currentAmmo = maxAmmo;
+        _isReloading = false;
+        input.reload = false;
+        input.shoot = false;
     }
 }
